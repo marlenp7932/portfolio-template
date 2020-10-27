c7152212c9e275b83579fd124757b391d8af28a0
marlenp7932 <73554006+marlenp7932@users.noreply.github.com>
2020-10-27 14:48:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Marlen P"
+let userName = "marlenp7932";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
